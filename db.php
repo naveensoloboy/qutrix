04c66845a2befa868ef77aa55dde39262b1750ec
Naveen <naveen9222777@gmail.com>
2025-03-03 00:03:10 +0530

    Initial commit

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "3705";
-$dbname = "qutrix";
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12765002";
+$password = "v6rMDHEyyw";
+$dbname = "sql12765002";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
